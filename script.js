e4c539ffd30b5127959aa16f78b082dfffc05878
hridoy2222 <59082827+hridoy2222@users.noreply.github.com>
2026-05-10 19:47:57 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("SGTRADECHART LIVE");
+function changePair(pair){
+
+  const chart =
+    document.getElementById("chartFrame");
+
+  chart.src =
+    `https://s.tradingview.com/widgetembed/?symbol=FX:${pair}&interval=1&theme=dark`;
+
+  const pairText =
+    document.getElementById("currentPair");
+
+  let displayName = pair;
+
+  if(pair === "EURUSD"){
+    displayName = "EUR/USD OTC";
+  }
+
+  if(pair === "USDBRL"){
+    displayName = "USD/BRL OTC";
+  }
+
+  if(pair === "GBPJPY"){
+    displayName = "GBP/JPY OTC";
+  }
+
+  if(pair === "AUDUSD"){
+    displayName = "AUD/USD OTC";
+  }
+
+  if(pair === "EURJPY"){
+    displayName = "EUR/JPY OTC";
+  }
+
+  pairText.innerText = displayName;
+
+}
